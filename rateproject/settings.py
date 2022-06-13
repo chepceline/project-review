e94cdded7fdfdca65a30a417cd953898f5f70f6d
celine <chepceline25@gmail.com>
2022-06-13 13:38:42 +0300

    add bootstrap

--- rateproject/settings.py
+++ rateproject/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rate',
+    'cloudinary',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config( 
+  cloud_name ="damaris",
+  api_key ="313514375783389",
+  api_secret ="XITkyvGW7haCwcrgfRBS78d4hnc"
+)
