5f06c3037a9f1aeda72e2f14ef044640430b3b30
celine <chepceline25@gmail.com>
2022-06-13 13:37:50 +0300

    add paths

---
+++ rate/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('auth/login/', views.login_user, name='login'),
+    path('auth/register/', views.register_user, name='register'),
+    path('accounts/logout/', views.logout_user, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('project/<project_id>', views.project, name='project'),
+    #path('api/profiles/', views.ProfileList.as_view()),
+    #path('api/projects/', views.ProjectsList.as_view())
+]
