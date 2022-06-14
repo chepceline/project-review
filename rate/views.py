33ef9bc7a67d695feb3044cb859e098b9606d684
celine <chepceline25@gmail.com>
2022-06-14 23:09:53 +0300

    add restframework

--- rate/views.py
+++ rate/views.py
@@ -4,10 +4,10 @@ from .forms import CreateUserForm, NewProjectForm, UpdateProfileForm, ReviewForm
 from django.contrib import messages
 from .models import Project, Review
 from django.contrib.auth.decorators import login_required
-#from rest_framework.response import Response
-#from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Profile
-
+from .serializer import ProfileSerializer, ProjectsSerializer
 # Create your views here.
 
 def index(request):
@@ -108,3 +108,16 @@ def project(request, project_id):
 
     return render(request, 'project.html', context)
 
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+
+        return Response(serializers.data)
+
+class ProjectsList(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectsSerializer(all_projects, many=True)
+
+        return Response(serializers.data)
